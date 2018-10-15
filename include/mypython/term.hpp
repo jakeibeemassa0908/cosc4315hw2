7e8467ec9b1645e5f0af12ef58b0d167bf923a80
Justin <jbtcao@gmail.com>
2018-10-15 15:36:25 -0500

    Organize term functions better

--- include/mypython/term.hpp
+++ include/mypython/term.hpp
@@ -11,30 +11,15 @@ struct Int;
 struct Bool;
 struct Str;
 
-using Term = mpark::variant<NoneType, Int, Bool, Str>;
-
-auto cmp(Term const& a, Term const& b) -> int;
-auto str(Term const& term) -> Str;
-auto truth_value(Term const& term) -> bool;
-
-auto add(Term const& a, Term const& b) -> Term;
-auto sub(Term const& a, Term const& b) -> Term;
-auto mul(Term const& a, Term const& b) -> Term;
-auto div(Term const& a, Term const& b) -> Term;
+using Term = mpark::variant<NoneType, Bool, Int, Str>;
 
 struct Str {
   std::string value = "";
 
   Str() = default;
   Str(std::string const& value) : value(value) {}
+  Str(char const* cstr) : Str(std::string(cstr)) {}
 };
-
-auto cmp(Str const& a, Str const& b) -> int;
-auto str(Str const& str) -> Str;
-auto truth_value(Str const& str) -> bool;
-
-auto add(Str const& a, Str const& b) -> Term;
-auto mul(Str const& a, Int const& b) -> Term;
 
 struct Int {
   long value = 0;
@@ -43,22 +28,37 @@ struct Int {
   Int(long value) : value(value) {}
 };
 
-auto cmp(Int const& a, Int const& b) -> int;
-auto str(Int const& i) -> Str;
-auto truth_value(Int const& i) -> bool;
-
-auto add(Int const& a, Int const& b) -> Term;
-auto sub(Int const& a, Int const& b) -> Term;
-auto mul(Int const& a, Int const& b) -> Term;
-auto div(Int const& a, Int const& b) -> Term;
-
 struct Bool : Int {};
-
-auto str(Bool const& b) -> Str;
 
 struct NoneType {};
 
+auto add(Term const& a, Term const& b) -> Term;
+auto add(Int const& a, Int const& b) -> Term;
+auto add(Str const& a, Str const& b) -> Term;
+
+auto cmp(Term const& a, Term const& b) -> int;
+auto cmp(Str const& a, Str const& b) -> int;
+auto cmp(Int const& a, Int const& b) -> int;
+
+auto div(Term const& a, Term const& b) -> Term;
+auto div(Int const& a, Int const& b) -> Term;
+
+auto mul(Term const& a, Term const& b) -> Term;
+auto mul(Str const& a, Int const& b) -> Term;
+auto mul(Int const& a, Int const& b) -> Term;
+
+auto str(Term const& term) -> Str;
 auto str(NoneType const& n) -> Str;
+auto str(Bool const& b) -> Str;
+auto str(Int const& i) -> Str;
+auto str(Str const& str) -> Str;
+
+auto sub(Term const& a, Term const& b) -> Term;
+auto sub(Int const& a, Int const& b) -> Term;
+
+auto truth_value(Term const& term) -> bool;
+auto truth_value(Int const& i) -> bool;
+auto truth_value(Str const& str) -> bool;
 }  // namespace MyPython
 
 #endif
--- src/mypython/term.cpp
+++ src/mypython/term.cpp
@@ -41,53 +41,17 @@ auto div(T1 const& a, T2 const& b) -> Term {
 }
 }  // namespace
 
-auto cmp(Term const& a, Term const& b) -> int {
-  auto visitor = [](auto&& a, auto&& b) { return cmp(a, b); };
-  return mpark::visit(visitor, a, b);
-}
-
-auto str(Term const& term) -> Str {
-  auto visitor = [](auto&& term) { return str(term); };
-  return mpark::visit(visitor, term);
-}
-
-auto truth_value(Term const& term) -> bool {
-  auto visitor = [](auto&& term) { return truth_value(term); };
-  return mpark::visit(visitor, term);
-}
-
 auto add(Term const& a, Term const& b) -> Term {
   auto visitor = [](auto&& a, auto&& b) { return add(a, b); };
   return mpark::visit(visitor, a, b);
 }
 
-auto sub(Term const& a, Term const& b) -> Term {
-  auto visitor = [](auto&& a, auto&& b) { return sub(a, b); };
-  return mpark::visit(visitor, a, b);
-}
-
-auto mul(Term const& a, Term const& b) -> Term {
-  auto visitor = [](auto&& a, auto&& b) { return mul(a, b); };
-  return mpark::visit(visitor, a, b);
-}
-
-auto div(Term const& a, Term const& b) -> Term {
-  auto visitor = [](auto&& a, auto&& b) { return div(a, b); };
-  return mpark::visit(visitor, a, b);
-}
-
-auto cmp(Str const& a, Str const& b) -> int { return a.value.compare(b.value); }
-
-auto str(Str const& str) -> Str { return str; }
-auto truth_value(Str const& str) -> bool { return !str.value.empty(); }
-
+auto add(Int const& a, Int const& b) -> Term { return Int(a.value + b.value); }
 auto add(Str const& a, Str const& b) -> Term { return Str(a.value + b.value); }
-auto mul(Str const& a, Int const& b) -> Term {
-  Str result;
-  for (int i = 0; i < b.value; ++i) {
-    result.value += a.value;
-  }
-  return result;
+
+auto cmp(Term const& a, Term const& b) -> int {
+  auto visitor = [](auto&& a, auto&& b) { return cmp(a, b); };
+  return mpark::visit(visitor, a, b);
 }
 
 auto cmp(Int const& a, Int const& b) -> int {
@@ -99,23 +63,53 @@ auto cmp(Int const& a, Int const& b) -> int {
     return 0;
 }
 
-auto str(Int const& i) -> Str { return Str(std::to_string(i.value)); }
-auto truth_value(Int const& i) -> bool { return i.value != 0; }
+auto cmp(Str const& a, Str const& b) -> int { return a.value.compare(b.value); }
 
-auto add(Int const& a, Int const& b) -> Term { return Int(a.value + b.value); }
-auto sub(Int const& a, Int const& b) -> Term { return Int(a.value - b.value); }
-auto mul(Int const& a, Int const& b) -> Term { return Int(a.value * b.value); }
-auto div(Int const& a, Int const& b) -> Term { return Int(a.value / b.value); }
-
-auto str(Bool const& b) -> Str {
-  if (b.value != 0) {
-    return Str("True");
-  } else {
-    return Str("False");
-  }
+auto div(Term const& a, Term const& b) -> Term {
+  auto visitor = [](auto&& a, auto&& b) { return div(a, b); };
+  return mpark::visit(visitor, a, b);
 }
 
-auto str(NoneType const& n) -> Str { return Str("None"); }
+auto div(Int const& a, Int const& b) -> Term { return Int(a.value / b.value); }
 
-////
+auto mul(Term const& a, Term const& b) -> Term {
+  auto visitor = [](auto&& a, auto&& b) { return mul(a, b); };
+  return mpark::visit(visitor, a, b);
+}
+
+auto mul(Int const& a, Int const& b) -> Term { return Int(a.value * b.value); }
+
+auto mul(Str const& a, Int const& b) -> Term {
+  Str result;
+  for (int i = 0; i < b.value; ++i) {
+    result.value += a.value;
+  }
+  return result;
+}
+
+auto str(Term const& term) -> Str {
+  auto visitor = [](auto&& term) { return str(term); };
+  return mpark::visit(visitor, term);
+}
+
+auto str(Int const& i) -> Str { return std::to_string(i.value); }
+auto str(Str const& str) -> Str { return str; }
+auto str(NoneType const& n) -> Str { return "None"; }
+auto str(Bool const& b) -> Str { return (b.value != 0) ? "True" : "False"; }
+
+auto sub(Term const& a, Term const& b) -> Term {
+  auto visitor = [](auto&& a, auto&& b) { return sub(a, b); };
+  return mpark::visit(visitor, a, b);
+}
+
+auto sub(Int const& a, Int const& b) -> Term { return Int(a.value - b.value); }
+
+auto truth_value(Term const& term) -> bool {
+  auto visitor = [](auto&& term) { return truth_value(term); };
+  return mpark::visit(visitor, term);
+}
+
+auto truth_value(NoneType const& n) -> bool { return false; }
+auto truth_value(Str const& str) -> bool { return !str.value.empty(); }
+auto truth_value(Int const& i) -> bool { return i.value != 0; }
 }  // namespace MyPython
